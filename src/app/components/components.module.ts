2f487840c8fe721d7df5b68d84a6a8dd43be9262
dmytro <kuzdsma1998@gmail.com>
2022-11-12 19:53:48 +0200

    add photo creation

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageSettingsComponent } from './image-settings/image-settings.component';
-import { PreviewComponent } from './preview/preview.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [CommonModule, MatTabsModule, MatIconModule],
-  declarations: [ImageSettingsComponent, PreviewComponent],
-  exports: [ImageSettingsComponent, PreviewComponent],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
+  declarations: [ImageSettingsComponent],
+  exports: [ImageSettingsComponent],
 })
 export class ComponentsModule {}
--- src/app/components/image-settings/image-settings.component.ts
+++ src/app/components/image-settings/image-settings.component.ts
@@ -1,12 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  TemplateRef,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-image-settings',
   templateUrl: './image-settings.component.html',
   styleUrls: ['./image-settings.component.scss'],
 })
-export class ImageSettingsComponent implements OnInit {
-  constructor() {}
+export class ImageSettingsComponent implements OnInit, AfterViewInit {
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {}
+  @ViewChild('preview') preview?: ElementRef<any>;
+
+  form?: FormGroup;
+  imgUrl?: string | ArrayBuffer;
+  imgName?: string;
+  animationTypes: any[] = [
+    'slide in from top',
+    'zoom in from bottom',
+    'left to right',
+    'right to left',
+  ];
+
+  ngOnInit() {
+    this.setForm();
+  }
+
+  ngAfterViewInit() {
+    console.log(this.preview?.nativeElement);
+  }
+
+  setForm() {
+    this.form = this.formBuilder.group({
+      imageFile: [, Validators.required],
+      animationType: [],
+      imageSize: [],
+      containerWidth: [],
+      containerHeight: [],
+      horizontalPosition: [],
+      verticalPosition: [],
+    });
+  }
+
+  uploadImageFile(file: any) {
+    this.imgName = file[0].name;
+    this.form?.patchValue({
+      imageFile: [file[0]],
+    });
+    const reader = new FileReader();
+    reader.readAsDataURL(file[0]);
+    reader.onload = (event) => {
+      if (reader.result !== null) {
+        this.imgUrl = reader.result;
+      }
+    };
+    console.log(this.preview?.nativeElement);
+  }
+
+  clearImgList() {
+    this.imgName = '';
+    this.imgUrl = '';
+  }
+
+  onSubmit() {}
 }
